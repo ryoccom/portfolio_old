3d858431f2fac66b18486acddf128daa28fc8289
ryoccom <47664789+ryoccom@users.noreply.github.com>
2022-02-07 22:30:44 +0900

    3画面目でった

--- asset_src/js/modules/scroll.js
+++ asset_src/js/modules/scroll.js
@@ -1,3 +1,25 @@
-//明日はJs描こうかな
+import { gsap } from "gsap";
+import ScrollTrigger from 'gsap/ScrollTrigger';
 
+gsap.registerPlugin(ScrollTrigger);
+
+//ningen scroll trigger
+gsap.to('.js-ningen', { 
+    bottom: '-90%',
+    scale:'.7',
+    scrollTrigger: {
+      trigger: '.top',
+      start: 'top top',
+      end: 'bottom 20%',
+      scrub: true,
+    }
+});
+
+gsap.to('.about', { 
+    scrollTrigger: {
+        trigger: '.about',
+        toggleClass:'js-active',
+        start: 'top 20%',
+    }
+});
 
