7a72b4a29aac74ec1c2462314a5892d351718341
ryoccom <47664789+ryoccom@users.noreply.github.com>
2022-02-14 23:33:31 +0900

    SP化してみた

--- asset_src/js/modules/scrolltrigger.js
+++ asset_src/js/modules/scrolltrigger.js
@@ -1,4 +1,6 @@
-import { gsap } from "gsap";
+import {
+  gsap
+} from "gsap";
 import ScrollTrigger from 'gsap/ScrollTrigger';
 
 gsap.registerPlugin(ScrollTrigger);
@@ -10,55 +12,56 @@ ScrollTrigger.defaults({
 });
 
 //ningen scroll trigger
-gsap.to('.js-ningen', { 
-    bottom: '-105%',
-    scale:'.7',
-    scrollTrigger: {
-      trigger: '.top',
-      start: 'top top',
-      end: 'bottom 20%',
-      scrub: true,
-      onLeave: () => {
-        ningen.style.zIndex = '1000';
-      },
-      onEnterBack: () => {
-        ningen.style.zIndex = '3000';
-      },
-    }
-});
 
-gsap.to('.about', { 
-    scrollTrigger: {
-        trigger: '.about',
-        toggleClass:'js-active',
-        start: 'top 20%',
-    }
-});
-
-gsap.to('.js-fade', { 
-  opacity: 1,
+gsap.to('.js-ningen', {
+  bottom: '-212%',
+  scale: '.7',
   scrollTrigger: {
-      trigger: '.works',
-      start: 'top top',
-      end: 'bottom bottom',
-      scrub: true
+    trigger: '.top',
+    start: 'top top',
+    end: 'bottom 20%',
+    scrub: true,
+    onLeave: () => {
+      ningen.style.zIndex = '1000';
+    },
+    onEnterBack: () => {
+      ningen.style.zIndex = '3000';
+    },
   }
 });
-gsap.to('.js-ningen2', { 
+
+gsap.to('.about', {
+  scrollTrigger: {
+    trigger: '.about',
+    toggleClass: 'js-active',
+    start: 'top 20%',
+  }
+});
+
+gsap.to('.js-fade', {
+  opacity: 1,
+  scrollTrigger: {
+    trigger: '.works',
+    start: 'top 20%',
+    end: 'bottom bottom',
+    scrub: true
+  }
+});
+gsap.to('.js-ningen2', {
   top: '50%',
   opacity: 1,
   scrollTrigger: {
-      trigger: '.works',
-      start: 'top 20%',
-      end: 'bottom bottom',
-      scrub: true,
+    trigger: '.works',
+    start: 'top 20%',
+    end: 'bottom bottom',
+    scrub: true,
   }
 });
 
-gsap.to('.works', { 
+gsap.to('.works', {
   scrollTrigger: {
-      trigger: '.works',
-      toggleClass:'js-active',
-      start: 'top 20%',
+    trigger: '.works',
+    toggleClass: 'js-active',
+    start: 'top 20%',
   }
 });
